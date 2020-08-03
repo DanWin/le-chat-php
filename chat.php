5d4a7aba2c1d3b1594ebc59a3589b5d677adeff2
navnit0707 <knavnit81@gmail.com>
2020-08-03 20:37:13 +0530

    setting style of a to text decoration none

--- chat.php
+++ chat.php
@@ -254,7 +254,7 @@ function print_stylesheet($init=false){
 	//default css
 	echo '<style type="text/css">';
 	echo 'body{background-color:#000000;color:#FFFFFF;font-size:14px;text-align:center;} ';
-	echo 'a:visited{color:#B33CB4;} a:active{color:#FF0033;} a:link{color:#0000FF;} #messages{word-wrap:break-word;} ';
+	echo 'a:visited{text-decoration: none;color:#B33CB4;} a:active{text-decoration: none;color:#FF0033;} a:link{text-decoration: none;color:#0000FF;} #messages{word-wrap:break-word;} ';
 	echo 'input,select,textarea{color:#FFFFFF;background-color:#000000;} .messages a img{width:15%} .messages a:hover img{width:35%} ';
 	echo '.error{color:#FF0033;text-align:left;} .delbutton{background-color:#660000;} .backbutton{background-color:#004400;} #exitbutton{background-color:#AA0000;} ';
 	echo '.setup table table,.admin table table,.profile table table{width:100%;text-align:left} ';
