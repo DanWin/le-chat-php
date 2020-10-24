cbd07f2f571bd9f1688f5af12983acf140639730
Daniel Winzen <daniel@danwin1210.me>
2020-10-24 21:43:23 +0200

    Unquote font-family names where applicalable

--- chat.php
+++ chat.php
@@ -4186,18 +4186,18 @@ function check_db(){
 
 function load_fonts() : array {
 	return [
-		'Arial'			=>"font-family:'Arial','Helvetica','sans-serif';",
-		'Book Antiqua'		=>"font-family:'Book Antiqua','MS Gothic';",
-		'Comic'			=>"font-family:'Comic Sans MS','Papyrus';",
-		'Courier'		=>"font-family:'Courier New','Courier','monospace';",
-		'Cursive'		=>"font-family:'Cursive','Papyrus';",
-		'Fantasy'		=>"font-family:'Fantasy','Futura','Papyrus';",
-		'Garamond'		=>"font-family:'Garamond','Palatino','serif';",
-		'Georgia'		=>"font-family:'Georgia','Times New Roman','Times','serif';",
-		'Serif'			=>"font-family:'MS Serif','New York','serif';",
-		'System'		=>"font-family:'System','Chicago','sans-serif';",
-		'Times New Roman'	=>"font-family:'Times New Roman','Times','serif';",
-		'Verdana'		=>"font-family:'Verdana','Geneva','Arial','Helvetica','sans-serif';",
+		'Arial'			=>"font-family:Arial,Helvetica,sans-serif;",
+		'Book Antiqua'		=>"font-family:'Book Antiqua','MS Gothic',serif;",
+		'Comic'			=>"font-family:'Comic Sans MS',Papyrus,sans-serif;",
+		'Courier'		=>"font-family:'Courier New',Courier,monospace;",
+		'Cursive'		=>"font-family:Cursive,Papyrus,sans-serif;",
+		'Fantasy'		=>"font-family:Fantasy,Futura,Papyrus,sans;",
+		'Garamond'		=>"font-family:Garamond,Palatino,serif;",
+		'Georgia'		=>"font-family:Georgia,'Times New Roman',Times,serif;",
+		'Serif'			=>"font-family:'MS Serif','New York',serif;",
+		'System'		=>"font-family:System,Chicago,sans-serif;",
+		'Times New Roman'	=>"font-family:'Times New Roman',Times,serif;",
+		'Verdana'		=>"font-family:Verdana,Geneva,Arial,Helvetica,sans-serif;",
 	];
 }
 
