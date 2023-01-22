b2c8aea0fd3434917ab20e532a88945f4a90d9d7
Daniel Winzen <daniel@danwin1210.de>
2023-01-22 20:37:48 +0100

    Parse sources as UTF-8

--- update-translation.sh
+++ update-translation.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-xgettext -o locale/le-chat-php.pot `find . -iname '*.php'`
+xgettext --from-code UTF-8 -o locale/le-chat-php.pot `find . -iname '*.php'`
 for translation in `find locale -iname '*.po'`; do msgmerge -U "$translation" locale/le-chat-php.pot; msgfmt -o ${translation:0:-2}mo "$translation"; done
